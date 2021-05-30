c3bf7d2fd1c20b6ff018dc006ea156a6c849ca52
Yondoku <85082159+on-time-0@users.noreply.github.com>
2021-05-30 14:41:19 -0800

    xcvdsdfbgrefb vfded

--- timer.js
+++ timer.js
@@ -11,14 +11,13 @@ let x = setInterval(() => {
   let distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24)),
+  hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+  minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+  seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s";
+  document.getElementById("demo").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
 
   // If the count down is finished, write some text
   if (distance < 0) {
